3837c2589d4572ec91ce304b51f226406a84e3c3
Jackie Chui <jachui@microsoft.com>
2019-03-07 20:48:24 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const replaceWithComponent = async () => {
+	const selectedNodes = figmaPlus.scene.selection;
+	const newNodes = [];
+	for (let selectedNode of selectedNodes) {
+		figmaPlus.scene.selection = [selectedNode];
+		App.triggerAction('create-symbol');
+		App.triggerAction('duplicate-in-place');
+		await new Promise(resolve => setTimeout(resolve, 30));
+		const newNode = await figmaPlus.scene.selection[0].getProperties();
+		newNodes.push(newNode);
+		App.updateSelectionProperties({ width: newNode.width + 1 });
+		App.updateSelectionProperties({ width: newNode.width });
+		App.triggerAction('select-next-sibling');
+		App.triggerAction('delete-selection');
+	}
+	figmaPlus.scene.selection = newNodes;
+	const instanceDropdown = document.querySelector('div[class*="instance_panel--symbolSelect--"]');
+	App._dispatch({
+		type: 'SHOW_DROPDOWN',
+		payload: {
+			type: 'instance-symbol-select',
+			data: {
+				clientRect: instanceDropdown.getBoundingClientRect()
+			}
+		}
+	});
+};
+
+figmaPlus.createPluginsMenuItem('Replace with Component', replaceWithComponent);
+
+figmaPlus.createContextMenuItem.Selection('Replace with Component', replaceWithComponent);
