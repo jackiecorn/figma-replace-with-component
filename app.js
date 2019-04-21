b8361d16f8f5ba652183bda2e3b9adfd1b79cd54
Jackie Chui <jackie2009hk@gmail.com>
2019-04-21 14:34:45 -0700

    Updated to new API

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const replaceWithComponent = async () => {
-	const selectedNodes = figmaPlus.scene.selection;
+const replace = async () => {
+	const selectedNodes = figmaPlus.currentPage.selection;
 	const newNodes = [];
 	for (let selectedNode of selectedNodes) {
-		figmaPlus.scene.selection = [selectedNode];
+		figmaPlus.currentPage.selection = [selectedNode];
 		App.triggerAction('create-symbol');
 		App.triggerAction('duplicate-in-place');
 		await new Promise(resolve => setTimeout(resolve, 30));
-		const newNode = await figmaPlus.scene.selection[0].getProperties();
+		const newNode = await figmaPlus.currentPage.selection[0].getProperties();
 		newNodes.push(newNode);
-		App.updateSelectionProperties({ width: newNode.width + 1 });
-		App.updateSelectionProperties({ width: newNode.width });
 		App.triggerAction('select-next-sibling');
 		App.triggerAction('delete-selection');
 	}
-	figmaPlus.scene.selection = newNodes;
+	figmaPlus.currentPage.selection = newNodes;
 	const instanceDropdown = document.querySelector('div[class*="instance_panel__REFRESH--symbolSelect--"]');
 	App._dispatch({
 		type: 'SHOW_DROPDOWN',
@@ -26,6 +24,36 @@ const replaceWithComponent = async () => {
 	});
 };
 
-figmaPlus.createPluginsMenuItem('Replace with Component', replaceWithComponent);
+const replaceAndKeepSize = async () => {
+	const selectedNodes = figmaPlus.currentPage.selection;
+	const newNodes = [];
+	for (let selectedNode of selectedNodes) {
+		figmaPlus.currentPage.selection = [selectedNode];
+		App.triggerAction('create-symbol');
+		App.triggerAction('duplicate-in-place');
+		await new Promise(resolve => setTimeout(resolve, 30));
+		const newNode = await figmaPlus.currentPage.selection[0].getProperties();
+		newNodes.push(newNode);
+		App.updateSelectionProperties({ width: newNode.width + 1 });
+		App.updateSelectionProperties({ width: newNode.width });
+		App.triggerAction('select-next-sibling');
+		App.triggerAction('delete-selection');
+	}
+	figmaPlus.currentPage.selection = newNodes;
+	const instanceDropdown = document.querySelector('div[class*="instance_panel__REFRESH--symbolSelect--"]');
+	App._dispatch({
+		type: 'SHOW_DROPDOWN',
+		payload: {
+			type: 'instance-symbol-select',
+			data: {
+				clientRect: instanceDropdown.getBoundingClientRect()
+			}
+		}
+	});
+};
 
-figmaPlus.createContextMenuItem.Selection('Replace with Component', replaceWithComponent);
+figmaPlus.addCommand({
+	label: 'Replace with Component',
+	submenu: [{ label: 'Replace', action: replace }, { label: 'Replace and Keep Size', action: replaceAndKeepSize }],
+	showInSelectionMenu: true
+});
